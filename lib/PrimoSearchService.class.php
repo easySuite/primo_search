05f9ea74e86dd1580cb02558527f33f6158b0e9d
Nicolae Grigoriu <ng@inlead.dk>
2016-01-25 07:59:46 +0200

    DAB-2 - Add PrimoSearchService class for service integration.

---
+++ lib/PrimoSearchService.class.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * @file
+ * Main functions to get the results from Primo Search service.
+ */
+
+class PrimoSearchService {
+  // Service url.
+  private $url;
+
+  public function __construct($url) {
+    $this->url = $url;
+  }
+
+  /**
+   * Find elements based on request.
+   *
+   * @param $params
+   *   Contains default parameters for find query: code, base, request.
+   *
+   * @return \SimpleXMLElement
+   *   Object of the number of the set and records.
+   */
+  public function findPrimoItems($params) {
+    $params['op'] = 'find';
+    return $this->getPrimoItems($params);
+  }
+
+  /**
+   * Present/Load items based on set number returned from find request.
+   *
+   * @param $params
+   *   Parameters that include set number of item and amount of items to load,
+   *   contains default keys: format, set_no, set_entry.
+   *
+   * @return array
+   *   Array of filtered items from service.
+   */
+  public function presentPrimoItems($params) {
+    $params['op'] = 'present';
+    $results = $this->getPrimoItems($params);
+    $items = $this->filterPrimoItems($results);
+
+    return $items;
+  }
+
+  /**
+   * Unset unnecessary attributes of the XMLObject.
+   *
+   * @param $results
+   *   XMLObject of the primo search result.
+   *
+   * @return array
+   *   Filtered array of primo search result items.
+   */
+  private function filterPrimoItems($results) {
+    $items = array();
+    $i = 0;
+    foreach ($results as $res) {
+      $metadata = $res->metadata;
+      $items[$i]['attributes'] = $metadata->oai_marc;
+
+      $doc_number = $res->doc_number;
+      if (!empty((string)$doc_number[0])) {
+        $items[$i]['doc_number'] = (string) $doc_number[0];
+      }
+      $i++;
+    }
+
+    // Unset the last element of array because it is the session id.
+    array_pop($items);
+
+    return $items;
+  }
+
+  /**
+   * Build query based on parameters that are passed from outside of class.
+   *
+   * @param $params
+   *   array of GET params for query.
+   *
+   * @return \SimpleXMLElement
+   *   Object of Primo Search item based on operation key of the params array.
+   */
+  private function getPrimoItems($params) {
+    $query = drupal_http_build_query($params);
+    $request = implode('?', array($this->url, urldecode($query)));
+    $response = drupal_http_request($request);
+    $result = simplexml_load_string($response->data);
+
+    return $result;
+  }
+}
