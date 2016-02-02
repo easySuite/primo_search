f9d1b6adfa96afb32bc1028381daa19fabcf8a3b
Nicolae Grigoriu <ng@inlead.dk>
2016-02-02 18:16:58 +0200

    DAB-2 - Add carousel functionality.

---
+++ templates/primo-search-carousel.tpl.php
@@ -0,0 +1,31 @@
+<div class="rs-carousel-wrapper">
+  <div class="rs-carousel">
+    <div class="rs-carousel-inner">
+      <div class="ajax-loader"></div>
+      <div class="rs-carousel-title"></div>
+      <div class="rs-carousel-items">
+        <ul class="rs-carousel-runner"></ul>
+      </div>
+    </div>
+    <?php if (count($tabs) > 1): ?>
+      <div class="rs-carousel-tabs">
+        <ul class="rs-carousel-list-tabs">
+          <?php foreach ($tabs as $key => $tab): ?>
+            <li class="rs-carousel-item">
+              <a href="#"><?php print $tab; ?></a>
+            </li>
+          <?php endforeach; ?>
+        </ul>
+
+        <!-- Used for responsive -->
+        <select class="rs-carousel-select-tabs">
+          <?php foreach ($tabs as $tab): ?>
+            <option class="rs-carousel-item">
+              <?php print $tab; ?>
+            </option>
+          <?php endforeach; ?>
+        </select>
+      </div>
+    <?php endif; ?>
+  </div>
+</div>
--- templates/primo-search-item.tpl.php
+++ templates/primo-search-item.tpl.php
@@ -3,7 +3,7 @@
   <div class="primo-content">
     <h2 class="primo-title">
       <a href="<?php print $title_link; ?>" target="_blank">
-        <span class="category"><?php print $category; ?> </span><span class="title"><?php print $title; ?></span>
+        <span class="category"><?php print $category; ?></span><span>: </span><span class="title"><?php print $title; ?></span>
       </a>
     </h2>
     <?php if ($author_name && $author_surname): ?>
